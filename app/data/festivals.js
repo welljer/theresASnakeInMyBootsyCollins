d1074e6383b8808a6c71cc2624e6465c03d608f9
Erin Hancock <welljer@gmail.com>
2019-02-11 22:31:49 -0700

    weAreWater

--- app/data/festivals.js
+++ app/data/festivals.js
@@ -1,35 +1,35 @@
 var musicFestival = [
-{ 
-    name: "Resonance Music and Arts Festival", 
-    photo: "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/resonancefest.jpg", 
-    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] 
-}, 
-{ 
-    "name": "Kinetic Fire Flow Arts Festival", 
-    "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/kineticFire.jpg", 
-    "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] 
-}, 
-{ 
-    "name": "Summer Camp Music Festival", 
-    "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/summerCamp.jpg", 
-    "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] 
-}, 
-{ 
-    "name": "Rootwire", 
-    "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/rootWire.jpg", 
-    "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] 
-}, 
-{ 
-    "name": "The Werkout Festival", 
-    "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/werkOut.jpg", 
-    "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] 
-}, 
-{ 
-    "name": "Hookahville", 
-    "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/hookahville.jpg", 
-    "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] 
- }
-];
-
-// export file
-module.exports = musicFestival;
+    { 
+        name: "Resonance Music and Arts Festival", 
+        photo: "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/resonancefest.jpg", 
+        scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] 
+    }, 
+    { 
+        "name": "Kinetic Fire Flow Arts Festival", 
+        "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/kineticFire.jpg", 
+        "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] 
+    }, 
+    { 
+        "name": "Summer Camp Music Festival", 
+        "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/summerCamp.jpg", 
+        "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] 
+    }, 
+    { 
+        "name": "Rootwire", 
+        "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/rootWire.jpg", 
+        "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] 
+    }, 
+    { 
+        "name": "The Werkout Festival", 
+        "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/werkOut.jpg", 
+        "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] 
+    }, 
+    { 
+        "name": "Hookahville", 
+        "photo": "https://github.com/welljer/theresASnakeInMyBootsyCollins/blob/master/app/data/media/hookahville.jpg", 
+        "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] 
+     }
+    ];
+    
+    // export file
+    module.exports = musicFestival;
